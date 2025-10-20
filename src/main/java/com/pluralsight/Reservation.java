f412a4891f6344cf02ff09907b904bf38449bf01
Julian Palmgren <julianpalmgren@gmail.com>
2025-10-20 14:02:43 -0400

    Second commit, added constructors, getters, and setters to the different classes. Next is to code the logic in each class to work in main.

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,4 +1,53 @@
 package com.pluralsight;
 
 public class Reservation {
+    private String roomType;
+    private double price;
+    private int numberOfNights;
+    private boolean weekend;
+    private double reservationTotal;
+
+    public Reservation(String roomType, double price, int numberOfNights, boolean weekend, double reservationTotal) {
+        this.roomType = roomType;
+        this.price = price;
+        this.numberOfNights = numberOfNights;
+        this.weekend = weekend;
+        this.reservationTotal = reservationTotal;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return weekend;
+    }
+
+    public double getReservationTotal() {
+        return reservationTotal;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public void setWeekend(boolean weekend) {
+        this.weekend = weekend;
+    }
 }
--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,4 +1,44 @@
 package com.pluralsight;
 
 public class Room {
+    private int beds;
+    private double price;
+    private boolean occupied;
+    private boolean dirty;
+    private boolean available;
+
+    public Room(int beds, double price, boolean occupied, boolean dirty, boolean available) {
+        this.beds = beds;
+        this.price = price;
+        this.occupied = occupied;
+        this.dirty = dirty;
+        this.available = available;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
 }
+
+
+
+
+
+
+
