9f56fa0e65a1889235b4e1f61b59b96b4ec75e48
Julian Palmgren <julianpalmgren@gmail.com>
2025-10-22 14:39:16 -0400

    Ninth commit, filled in and completed Hotel class with 2 constructors and the methods for them. Now moving onto unit testing.

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -82,7 +82,7 @@ public class Employee {
 
         this.hoursWorked += (time - this.startTime);
 
-        this.startTime = 0;
+        this.startTime = -1;
 
     }
 
@@ -107,7 +107,7 @@ public class Employee {
 
         this.hoursWorked += (hours - this.startTime);
 
-        this.startTime = 0;
+        this.startTime = -1;
 
     }
 
---
+++ src/test/java/com/pluralsight/RoomTest.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @org.junit.jupiter.api.Test
+    void checkIn() {
+
+
+
+    }
+}
